b43e1ef845e1cbf1e4f47d1059731cd34cd183d6
kylmioita <102211270+kylmioita@users.noreply.github.com>
2022-04-15 21:50:57 +0300

    animaatiot yms.

--- 2D-Peli/Assets/PlayerMovement.cs
+++ 2D-Peli/Assets/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Animator animator;
 
     Vector2 movement;
 
@@ -16,11 +17,27 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        bool flipped = movement.x < 0;
+        this.transform.rotation = Quaternion.Euler(new Vector3(0f, flipped ? 180f : 0f, 0f));
+
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (movement != Vector2.zero)
+        {
+            var xMovement = movement.x * moveSpeed * Time.deltaTime;
+            this.transform.Translate(new Vector3(xMovement, 0), Space.World);
+        }
     }
 
 }
+
+
