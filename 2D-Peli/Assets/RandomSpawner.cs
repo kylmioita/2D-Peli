b83d378996ecd0203d58bd0a316d1aeb67a78769
kylmioita <102211270+kylmioita@users.noreply.github.com>
2022-04-22 12:21:13 +0300

    sirkkeli spawnays

---
+++ 2D-Peli/Assets/RandomSpawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] enemyPrefabs;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            int randEnemy = Random.Range(0, enemyPrefabs.Length);
+            int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+
+            Instantiate(enemyPrefabs[randEnemy], spawnPoints[randSpawnPoint].position, transform.rotation);
+        }
+
+
+    }
+}
